d8d143282405cea553e6eb90d922d464b29b30d4
irorere juliet <irorerejuliet@gmail.com>
2025-11-27 23:37:02 +0100

    still working on womens collection

--- src/Components/Products/NewArrival.tsx
+++ src/Components/Products/NewArrival.tsx
@@ -1,24 +1,37 @@
 const NewArrival = () => {
     return (
-      <div>
+      <div className="">
         <div className="flex gap-4 items-center">
           <div className="bg-primary w-4 h-10 rounded-[5px]"></div>
           <p className="text-primary text-base font-semibold">Featured</p>
         </div>
         <h3 className="text-3xl font-semibold">New Arrival</h3>
-        <div className="relative">
-          <div className="bg-black text-white w-[570px] p-14">
-            <img src="/images/PlayStation-5.svg" alt="" className="" />
-            <div className=" absolute bottom-24 ">
-              <h5 className=" text-2xl font-semibold">PlayStation 5</h5>
-              <p className="text-sm font-normal w-[250px]">Black and White version of the PS5 coming out on sale.</p>
-              <p className="underline text-base font-medium ">Shop now</p>
+        <div className="flex justify-between">
+          <div className="relative">
+            <div className="bg-black text-white w-[570px] p-14">
+              <img src="/images/PlayStation-5.svg" alt="" className="" />
+              <div className=" absolute bottom-24 ">
+                <h5 className=" text-2xl font-semibold">PlayStation 5</h5>
+                <p className="text-sm font-normal w-[250px]">
+                  Black and White version of the PS5 coming out on sale.
+                </p>
+                <p className="underline text-base font-medium ">Shop now</p>
+              </div>
             </div>
           </div>
-          <div>
-            <img src="" alt="" />
+
+          <div className="relative">
+            <div className="bg-black text-white w-[570px] p-14">
+              <img src="/images/WomensCollection.svg" alt="" className="ml-20"/>
+              <div className=" absolute bottom-48 ">
+                <h5 className=" text-2xl font-semibold">PlayStation 5</h5>
+                <p className="text-sm font-normal w-[250px]">
+                  Black and White version of the PS5 coming out on sale.
+                </p>
+                <p className="underline text-base font-medium ">Shop now</p>
+              </div>
+            </div>
           </div>
-          <div></div>
         </div>
       </div>
     );
