978b7956379654f829b5d0c07ba053d0786688a4
irorere juliet <irorerejuliet@gmail.com>
2026-01-11 22:47:22 +0100

    Added a button to the flash sales

--- src/Components/Products/FlashSales.tsx
+++ src/Components/Products/FlashSales.tsx
@@ -6,8 +6,8 @@ const FlashSales = () => {
   return (
     <div className="my-40">
       <div className="flex gap-4 items-center">
-        <div className="bg-[#DB4444] w-4 h-10 rounded-[5px]"></div>
-        <p className="text-[#DB4444] text-base font-semibold">Today's</p>
+        <div className="bg-primary w-4 h-10 rounded-[5px]"></div>
+        <p className="text-primary text-base font-semibold">Today's</p>
       </div>
       <SalesTime />
       <div className="flex-row md:flex justify-between items-center gap-10 space-y-10 md:space-y-0 md:px-0 px-10">
@@ -40,14 +40,9 @@ const FlashSales = () => {
                   </Link>
                 </div>
               </div>
-              <button className="text-white bg-black w-[270px] py-2 rounded-t-none rounded-b-sm">
-                Add to cart{" "}
+              <button className="text-white bg-primary hover:bg-black w-[270px] py-2 rounded-t-none rounded-b-sm transform transition-transform duration-300 hover:scale-105">
+                Add to cart
               </button>
-              {/* {id === 2 && (
-                <button className="text-white bg-black w-[270px] py-2 rounded-t-none rounded-b-sm">
-                  Add to cart{" "}
-                </button>
-              )} */}
               {/* Product info below card */}
               <div className="mt-4 w-[210px]">
                 <p className="font-semibold text-lg">{description}</p>
@@ -72,7 +67,7 @@ const FlashSales = () => {
           )
         )}
       </div>
-      <button className="text-white bg-[#DB4444] text-base font-medium px-10 py-4 rounded-md my-16 w-[234px] md:mx-0 mx-10">
+      <button className="text-white bg-secondary text-base font-medium px-10 py-4 rounded-md my-16 w-[234px] md:mx-0 mx-10">
         View All Products
       </button>
       <div className="w-full border-t border-gray-200"></div>
