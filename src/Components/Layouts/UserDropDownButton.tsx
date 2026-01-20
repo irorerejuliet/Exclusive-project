d5b621e2bd640de4f2aae67168cad2599db02525
irorere juliet <irorerejuliet@gmail.com>
2026-01-20 22:22:27 +0100

    creating a userDropDown menu

--- src/Components/Layouts/UserDropDownButton.tsx
+++ src/Components/Layouts/UserDropDownButton.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { LuUser } from "react-icons/lu";
+import { Link } from 'react-router-dom';
 
 const UserDropDownButton = () => {
   return (
     <div>
-      
+      <div>
+        <Link to=""></Link>
+      </div>
     </div>
   )
 }
